7012772a2127939556b66aefff0a46db5faf30f4
Prince Narteh <prinarteh@gmail.com>
2022-05-11 22:41:51 +0000

    feat: adds filter by many fields

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PropertyType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDto } from './dtos/home.dto';
+
+interface GetHomeParams {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  propertyType?: PropertyType;
+}
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getHomes(): Promise<HomeResponseDto[]> {
+  async getHomes(filters: GetHomeParams): Promise<HomeResponseDto[]> {
     const homes = await this.prismaService.home.findMany({
       select: {
         id: true,
@@ -23,6 +33,7 @@ export class HomeService {
           take: 1,
         },
       },
+      where: filters,
     });
     return homes.map((home) => new HomeResponseDto(home));
   }
